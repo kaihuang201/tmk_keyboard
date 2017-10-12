d32bdb2f625e9f5584346ad506c5b1585bd8d460
kaihuang201 <kaihuang201@gmail.com>
2017-10-12 02:08:23 -0700

    Make CapsLock dual role. Hold activates number layer, tap sends ESC.

--- keyboard/satan/keymap_kai.c
+++ keyboard/satan/keymap_kai.c
@@ -46,7 +46,7 @@ uint8_t keymap_key_to_keycode(uint8_t layer, keypos_t key)
         {
             {   KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS },
             {   KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC },
-            {   KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,   KC_NO,  KC_ENT },
+            {   KC_FN3,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,   KC_NO,  KC_ENT },
             {   KC_FN5,  KC_FN1,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_FN2,  KC_FN6 },
             {  KC_LCTL, KC_LGUI, KC_LALT,   KC_NO,   KC_NO,  KC_SPC,   KC_NO,   KC_NO,   KC_NO,  KC_DEL,  KC_FN0, KC_RALT,  KC_APP, KC_RCTL }
         },
@@ -57,7 +57,7 @@ uint8_t keymap_key_to_keycode(uint8_t layer, keypos_t key)
         {
             {   KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_LBRC, KC_RBRC, KC_BSLS },
             {   KC_TAB, KC_QUOT, KC_COMM,  KC_DOT,    KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_SLSH,  KC_EQL, KC_BSPC },
-            {   KC_ESC,    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S, KC_MINS,   KC_NO,  KC_ENT },
+            {   KC_FN3,    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S, KC_MINS,   KC_NO,  KC_ENT },
             {   KC_FN5,  KC_FN1, KC_SCLN,    KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,  KC_FN2,  KC_FN6 },
             {  KC_LCTL, KC_LGUI, KC_LALT,   KC_NO,   KC_NO,  KC_SPC,   KC_NO,   KC_NO,   KC_NO,  KC_DEL,  KC_FN0, KC_RALT,  KC_APP, KC_RCTL }
         },
@@ -68,7 +68,7 @@ uint8_t keymap_key_to_keycode(uint8_t layer, keypos_t key)
         {
             {   KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS },
             {   KC_TAB,    KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,    KC_F,    KC_U,    KC_P, KC_SCLN, KC_LBRC, KC_RBRC, KC_BSPC },
-            {   KC_ESC,    KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_E,    KC_O,    KC_I, KC_QUOT,   KC_NO,  KC_ENT },
+            {   KC_FN3,    KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_E,    KC_O,    KC_I, KC_QUOT,   KC_NO,  KC_ENT },
             {   KC_FN5,  KC_FN1,    KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,    KC_L, KC_COMM,  KC_DOT, KC_SLSH,  KC_FN2,  KC_FN6 },
             {  KC_LCTL, KC_LGUI, KC_LALT,   KC_NO,   KC_NO,  KC_SPC,   KC_NO,   KC_NO,   KC_NO,  KC_DEL,  KC_FN0, KC_RALT,  KC_APP, KC_RCTL }
         },
@@ -145,6 +145,7 @@ action_t keymap_fn_to_action(uint8_t keycode)
         [0] = ACTION_LAYER_MOMENTARY(5),
         [1] = ACTION_LAYER_TAP_TOGGLE(6),
         [2] = ACTION_LAYER_TAP_TOGGLE(5),
+        [3] = ACTION_LAYER_TAP_KEY(6, KC_ESC), // Dual role, hold activates number layer, tap sends ESC.
 
         [5] = ACTION_MODS_ONESHOT(MOD_LSFT),
         [6] = ACTION_MODS_ONESHOT(MOD_RSFT),
