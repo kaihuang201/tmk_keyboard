eb93ea27602bd6aba331ba6632d421be716817d9
kaihuang201 <kaihuang201@gmail.com>
2017-10-28 15:04:28 -0700

    Changed teensy cicade 60% keyboard layers

--- keyboard/cicade60_teensy_lc/keymap_kai.c
+++ keyboard/cicade60_teensy_lc/keymap_kai.c
@@ -43,16 +43,30 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {   KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC },
         {   KC_FN2,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,  KC_DEL },
         {   KC_FN4,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_NO,  KC_FN5,  KC_FN1 },
-        {  KC_LCTL, KC_LGUI, KC_LALT,  KC_FN4,   KC_NO, KC_BSPC,   KC_NO,  KC_SPC,   KC_NO,  KC_FN0, KC_RALT,   KC_NO,  KC_APP, KC_RCTL }
+        {  KC_LCTL, KC_LGUI, KC_LALT,  KC_FN4,   KC_NO,  KC_FN0,   KC_NO,  KC_SPC,   KC_NO, KC_BSPC, KC_RALT,   KC_NO,  KC_APP, KC_RCTL }
     },
 
-    /* 0: Default Carpalx layer */
+    /* 1: default Carpalx QFMLWY base layer
+     * ,-----------------------------------------------------------.
+     * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|Del|
+     * |-----------------------------------------------------------|
+     * |Tab  |  Q|  F|  M|  L|  W|  Y|  U|  O|  B|  J|  [|  ]| BS  |
+     * |-----------------------------------------------------------|
+     * |Esc   |  D|  S|  T|  N|  R|  I|  A|  E|  H|  ;|  '|   Enter|
+     * |-----------------------------------------------------------|
+     * |Shift|Num|  Z|  V|  G|  C|  X|  P|  K|  ,|  .|  /|Shift|Fn2|
+     * |-----------------------------------------------------------|
+     * |Ctl | Sys| Alt| Shift  | BS|  Space    | Fn0| Alt| App| Ctl|
+     * `-----------------------------------------------------------'
+     */
+    //*
+    [1] =
     {
         {   KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS },
-        {   KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC },
-        {   KC_FN2,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,  KC_DEL },
-        {   KC_FN4,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_NO,  KC_FN5,  KC_FN1 },
-        {  KC_LCTL, KC_LGUI, KC_LALT,  KC_FN4,   KC_NO, KC_BSPC,   KC_NO,  KC_SPC,   KC_NO,  KC_FN0, KC_RALT,   KC_NO,  KC_APP, KC_RCTL }
+        {   KC_TAB,    KC_Q,    KC_F,    KC_M,    KC_L,    KC_W,    KC_Y,    KC_U,    KC_O,    KC_B,    KC_J, KC_LBRC, KC_RBRC, KC_BSPC },
+        {   KC_FN2,    KC_D,    KC_S,    KC_T,    KC_N,    KC_R,    KC_I,    KC_A,    KC_E,    KC_H, KC_SCLN, KC_QUOT,  KC_ENT,  KC_DEL },
+        {   KC_FN4,    KC_Z,    KC_V,    KC_G,    KC_C,    KC_X,    KC_P,    KC_K, KC_COMM,  KC_DOT, KC_SLSH,   KC_NO,  KC_FN5,  KC_FN1 },
+        {  KC_LCTL, KC_LGUI, KC_LALT,  KC_FN4,   KC_NO,  KC_FN0,   KC_NO,  KC_SPC,   KC_NO, KC_BSPC, KC_RALT,   KC_NO,  KC_APP, KC_RCTL }
     },
 
     /* 2: Function layer
@@ -68,11 +82,12 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    |    |    |        |   |           |    |    |    |    |
      * `-----------------------------------------------------------'
      */
+    [2] =
     {
-        {  KC_TRNS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_TRNS },
-        {  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP, KC_PGDN,  KC_END, KC_FN10, KC_FN11,  KC_INS },
-        {  KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, KC_SLCK, KC_TRNS, KC_TRNS, KC_TRNS },
-        {  KC_TRNS, KC_SLEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS },
+        {  KC_FN12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_SLEP },
+        {  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP, KC_PGDN,  KC_END, KC_TRNS, KC_FN10, KC_FN11,  KC_DEL },
+        {  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_SLCK, KC_TRNS, KC_TRNS,  KC_INS },
+        {  KC_TRNS, KC_SLEP, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_PSCR, KC_TRNS, KC_TRNS, KC_PAUS },
         {  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
     },
 
@@ -89,12 +104,23 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    |    |    |        |   |           |    |    |    |    |
      * `-----------------------------------------------------------'
      */
+    [3] =
     {
-        {  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_7,    KC_8,    KC_9, KC_PSLS, KC_PAST, KC_TRNS, KC_TRNS },
+        {  KC_TRNS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_7,    KC_8,    KC_9, KC_PSLS, KC_TRNS, KC_TRNS, KC_TRNS },
         {  KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_4,    KC_5,    KC_6, KC_PMNS, KC_TRNS, KC_TRNS, KC_TRNS },
         {  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_1,    KC_2,    KC_3, KC_PPLS, KC_TRNS, KC_TRNS, KC_TRNS },
-        {  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_P0, KC_PDOT, KC_PENT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
+        {  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_0, KC_PAST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
         {  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
+    },
+
+    // 4: special layer
+    [4] =
+    {
+        {  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
+        {  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
+        {  KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
+        {  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
+        {  KC_TRNS, KC_TRNS, KC_TRNS,  KC_SPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
     },
 
 };  // to test: KC_CAPS, KT_BTLD, KC_A
@@ -108,6 +134,8 @@ const action_t PROGMEM fn_actions[] = {
     [5] = ACTION_MODS_ONESHOT(MOD_RSFT),
 
     [10] = ACTION_DEFAULT_LAYER_SET(0),  // set qwerty layout
-    [11] = ACTION_DEFAULT_LAYER_SET(1),  // set dvorak layout
+    [11] = ACTION_DEFAULT_LAYER_SET(1),  // set calpalx layout
 
+    [12] = ACTION_LAYER_TOGGLE(4) // special layer
 };
+
